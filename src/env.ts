b237e70264afa013d90c3f917861f8db089e8d4e
SlowSnakPierre <38976602+SlowSnakPierre@users.noreply.github.com>
2026-02-18 16:26:53 +0100

    fix(AL-1352): Fix Rules branche Develop (#9)

--- src/env.ts
+++ src/env.ts
@@ -5,13 +5,16 @@ export const env = createEnv({
 	server: {
 		DATABASE_URL: z.string(),
 		NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+		SENTRY_AUTH_TOKEN: z.string(),
 	},
 
 	client: {
-		NEXT_PUBLIC_URL: z.string().url().default("http://localhost:3000"),
+		NEXT_PUBLIC_SENTRY_DSN: z.string(),
+		NEXT_PUBLIC_URL: z.string().default("http://localhost:3000"),
 	},
 
 	experimental__runtimeEnv: {
+		NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
 		NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
 	},
 
