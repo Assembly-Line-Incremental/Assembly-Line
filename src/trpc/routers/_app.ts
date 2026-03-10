60345b0e8cc3e5d0224334c4b7ae92cc7e43a3bb
SlowSnakPierre <38976602+SlowSnakPierre@users.noreply.github.com>
2026-03-10 10:30:32 +0100

    feat(AL-1359): Rework l'architecture du projet

--- src/trpc/routers/_app.ts
+++
@@ -1,8 +0,0 @@
-import { createTRPCRouter } from "../init";
-import { gameRouter } from "./game";
-
-export const appRouter = createTRPCRouter({
-	game: gameRouter,
-});
-
-export type AppRouter = typeof appRouter;
